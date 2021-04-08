72078349c80ee466000a28e9856fba1ce63f264e
giuseppe <placidagiuseppe@gmail.com>
2021-04-08 16:58:11 +0200

    ...

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 var app = new Vue({
   el: '#root',
   data: {
-    email: "",
     array_email: [],
+    boolean: false,
   },
   methods: {
     genera: function () {
+
       for (var i = 0; i < 10; i++) {
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
           .then((response) => {
             console.log(response.data.response),
             this.array_email.push(response.data.response) ;
+            if (i == 10) {
+              this.boolean = true;
+            }
           });
        }
+
 
     },
 
