bb08a906eb88765888e1c0bd5ca40ac49a2cc4a5
giuseppe <placidagiuseppe@gmail.com>
2021-04-09 10:05:16 +0200

    bonus completato

--- main.js
+++ main.js
@@ -3,17 +3,27 @@ var app = new Vue({
   data: {
     array_email: [],
   },
-  methods: {
-    genera: function () {
-      this.array_email=[];
+  // methods: {
+  //   genera: function () {
+  //     this.array_email=[];
+  //     for (let i = 0; i < 10; i++) {
+  //       axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+  //         .then((response) => {
+  //           console.log(response.data.response),
+  //           this.array_email.push(response.data.response) ;
+  //         });
+  //      }
+  //   },
+    mounted(){
+      // this.array_email=[];
       for (let i = 0; i < 10; i++) {
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
           .then((response) => {
-            console.log(response.data.response),
+            // console.log(response.data.response),
             this.array_email.push(response.data.response) ;
           });
        }
-    },
+    }
 
-  }
+
 });
